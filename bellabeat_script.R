195e9b6ec82ceab5a1f2a0f57e80bf010426bf17
jeffbai996 <j.bai@wustl.edu>
2022-07-21 17:13:24 -0700

    Added R script for project, removed raw markdown file, created new directory for output to GitHub Pages

---
+++ bellabeat_script.R
@@ -0,0 +1,85 @@
+# load required packages
+library(tidyverse)
+library(janitor)
+library(skimr)
+library(lubridate)
+
+# load .csv into data frames
+dailyActivity <- read_csv("datasets/dailyActivity.csv")
+sleepDay <- read_csv("datasets/sleepDay.csv")
+weightLogInfo <- read_csv("datasets/weightLogInfo.csv")
+
+# convert column names
+dailyActivity <- clean_names(dailyActivity)
+sleepDay <- clean_names(sleepDay)
+weightLogInfo <- clean_names (weightLogInfo)
+
+# identify duplicates
+get_dupes(dailyActivity)
+get_dupes(sleepDay)
+get_dupes(weightLogInfo)
+
+# remove duplicates
+sleepDay <- distinct(sleepDay)
+
+# remove invalid records
+dailyActivity <- dailyActivity %>% 
+  filter(total_steps >= 500)
+
+# identify outliers and bad columns
+skim_without_charts(dailyActivity, !id)
+skim_without_charts(sleepDay, !id)
+weightLogInfo %>% 
+  select(-c(id, log_id)) %>% 
+  skim_without_charts()
+
+# remove outliers and bad columns
+dailyActivity <- select(
+  dailyActivity, 
+  -c(logged_activities_distance, sedentary_active_distance)
+)
+weightLogInfo <- select(
+  weightLogInfo, 
+  -c(fat, log_id)
+)
+
+# change column data type to 'date'
+dailyActivity$activity_date <- as_date(dailyActivity$activity_date, format = "%m/%d/%Y")
+
+# new column for day of week name
+dailyActivity <- dailyActivity %>% 
+  mutate(weekday = wday(activity_date, label = TRUE))
+
+# ggplot total steps over day of week
+dailyActivity %>% 
+  group_by(weekday) %>% 
+  ggplot(aes(x = weekday, y = total_steps)) +
+  geom_boxplot() +
+  labs(
+    title = "Total Steps Taken by Day of Week",
+    x = "Day of Week",
+    y = "Total Steps Taken",
+    caption = "Time frame: April 12, 2016 to May 11, 2016"
+  )
+
+# calculated column for total time worn
+dailyActivity <- dailyActivity %>% 
+  mutate(total_time_worn = very_active_minutes + fairly_active_minutes + 
+           lightly_active_minutes + sedentary_minutes)
+
+# ggplot tracker use over day of week
+dailyActivity %>% 
+  group_by(weekday) %>% 
+  ggplot(aes(x = weekday, y = total_time_worn)) +
+  geom_violin() +
+  geom_boxplot(width = 0.1) +
+  labs(
+    title = "Tracker Use by Day of Week",
+    x = "Day of Week",
+    y = "Percentage of Day Worn",
+    caption = "Time frame: April 12, 2016 to May 11, 2016"
+  ) +
+  scale_y_continuous(
+    breaks = c(360, 720, 1080, 1440),
+    labels = c("25%", "50%", "75%", "100%")
+  )
